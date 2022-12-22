8a00011f18354514f4c994122945d0602a8ba1b8
Mossaab BOUDCHICHA <boudchicha23@hotmail.fr>
2022-12-22 22:49:43 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Split-up the Quotation management from the Sale orders
+------------------------------------------------------
+This module allows to split-up the management of Quotations from Sale Orders. 
---
+++ __manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Split-up the Quotation management from the Sale orders',
+    'version': '1.0.1',
+    'author':'Soft-integration',
+    'category': 'Sale',
+    'summary': 'Split-up the Quotation management from the Sale orders',
+    'description': "",
+    'depends': [
+        'sale'
+    ],
+    'data': [
+        'views/sale_views.xml'
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
